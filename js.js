3a26197034025aba820f536bee57c60adbcb200b
Anubhav Raj <anubhavraj21082002@gmail.com>
2022-07-11 19:12:45 +0530

    try

---
+++ js.js
@@ -0,0 +1,15 @@
+var n=document.createElement("span");
+n.innertext="Anubhav Raj";
+document.getElementById("js").appendChild(n);
+
+var roll=document.createElement("span");
+roll.innertext="201500119";
+document.getElementById("js").appendChild(roll);
+
+var coll=document.createElement("span");
+coll.innertext="GLA University";
+document.getElementById("js").appendChild(coll);
+
+var date=document.createElement("span");
+date.innertext="11-07-2022";
+document.getElementById("js").appendChild(date);
